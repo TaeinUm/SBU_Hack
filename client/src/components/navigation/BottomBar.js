19ee82635225221e9e5bd6f0bf93b8e39d0cfe3e
Yunah Kim <yunah.kim@stonybrook.edu>
2024-02-10 14:41:15 -0500

    Upload Image Page

--- client/src/components/navigation/BottomBar.js
+++ client/src/components/navigation/BottomBar.js
@@ -1,15 +1,31 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import "./navbar.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 const BottomBar = () => {
   const navigation = useNavigate();
   const [activeIcon, setActiveIcon] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
 
   // Function to handle icon click and update the active state
   const handleIconClick = (iconName) => {
     setActiveIcon(iconName);
   };
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const imageDataUrl = reader.result;
+        setSelectedImage(imageDataUrl);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(selectedImage);
+  // }, [selectedImage]);
 
   return (
     <div className="navigation-card">
@@ -32,7 +48,18 @@ const BottomBar = () => {
           navigation("/main");
         }}
       >
-        <i className=" bottombar_icon  bi bi-camera"></i>
+        <label htmlFor="file" className="camera_label">
+          <i
+            className=" bottombar_icon  bi bi-camera"
+            style={{ position: "absolute", left: "18px", top: "15px" }}
+          />
+        </label>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          style={{ position: "absolute", zIndex: 1 }}
+        />
       </div>
 
       {/* Third icon */}
