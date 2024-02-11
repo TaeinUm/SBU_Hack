e9c47642a75fe2cc0d7e4e80b69cf39567e23e31
TaeinUm <taein.um@stonybrook.edu>
2024-02-11 04:10:12 -0500

    Error fixed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,7 @@ import { uploadSingleImage } from "../utils/s3upload.js"; // Import the upload u
 // New function to upload image
 export const uploadReceiptImage = async (req, res) => {
   const userId = req.user._id; // Ensure this is provided by your auth middleware
-  const file = req; // Provided by multer-s3
+  const file = req.file; // Provided by multer-s3
   if (!file) {
     return res.status(400).json({ message: "No file uploaded!" });
   }
