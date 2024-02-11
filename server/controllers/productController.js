ce9edfddd20598001be3dabee4fef47f208b441f
TaeinUm <taein.um@stonybrook.edu>
2024-02-11 00:44:37 -0500

    Get product date modified

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,35 +1,36 @@
 import User from "../models/User.js";
 
 export const updateProductInfo = async (req, res) => {
-  const { userId, productId } = req.params; // Use productId instead of productIndex
-  const { productName, expdate, donatable } = req.body;
+    const { userId, productId } = req.params;
+    const { productName, expdate, donatable } = req.body;
 
-  try {
-    // Find the user and the product by ID
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    try {
+        // Find the user and the product by ID
+        const user = await User.findById(userId);
+        if (!user) {
+        return res.status(404).json({ message: "User not found" });
+        }
+
+        // Find the specific product to update
+        const product = user.products.id(productId);
+        if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+        }
+
+        // Update the product information
+        if (productName !== undefined) product.productName = productName;
+        // Convert expdate from string to Date type before updating
+        if (expdate !== undefined) product.expdate = new Date(expdate);
+        if (donatable !== undefined) product.donatable = donatable;
+
+        // Save the user document with updated product information
+        await user.save();
+        res.status(200).json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: error.message });
     }
-
-    // Find the specific product to update
-    const product = user.products.id(productId); // Mongoose's id method to find a subdocument by its _id
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    // Update the product information
-    if (productName !== undefined) product.productName = productName;
-    if (expdate !== undefined) product.expdate = expdate;
-    if (donatable !== undefined) product.donatable = donatable;
-
-    // Save the user document with updated product information
-    await user.save();
-    res.status(200).json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error.message });
-  }
-};
+};  
 
 export const deleteProduct = async (req, res) => {
   const { userId, productId } = req.params; // Use productId for deletion
