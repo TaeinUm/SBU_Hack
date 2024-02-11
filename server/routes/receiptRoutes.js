f59548b860a2097dd3b7bc7570eecbf2fafeb411
Yunah Kim <yunah.kim@stonybrook.edu>
2024-02-10 19:00:49 -0500

    -

--- server/routes/receiptRoutes.js
+++ server/routes/receiptRoutes.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import multer from 'multer';
-import { uploadReceipt } from '../controllers/receiptController.js';
+import express from "express";
+import multer from "multer";
+import { uploadReceipt } from "../controllers/receiptController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
-router.post('/', upload.single('img'), uploadReceipt);
+// router.post('/', upload.single('img'), uploadReceipt);
 
+router.route("/").post(protect, upload.single("img"), uploadReceipt);
 export default router;
